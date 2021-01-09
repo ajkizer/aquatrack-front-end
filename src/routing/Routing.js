3381233f266ac3a61a3ed2a831d435e1d14b1b6e
Anthony Kizer <anthony.kizer88@gmail.com>
2021-01-09 13:28:17 -0800

    added 404 page

--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -5,6 +5,7 @@ import { Container } from "react-bootstrap";
 import UserAuth from "../pages/Auth/UserAuth";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import PrivateRoute from "../routing/PrivateRoute";
+import NotFound from "../pages/NotFound/NotFound";
 const Routing = () => {
   return (
     <>
@@ -13,6 +14,7 @@ const Routing = () => {
         <Switch>
           <Route exact path="/" component={UserAuth} />
           <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <Route path="*" component={NotFound} />
         </Switch>
       </Container>
     </>
